7b5d3830a09bd81168469142ad3a2f1aa49a4016
onlulatte <onlulatte@naver.com>
2023-08-07 15:33:03 +0900

    불필요한 코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import Main from "./components/Main";
 import Quiz from "./components/Quiz";
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./Main.scss";
+import "./Main.scss";
 
 const Main = () => {
   const navigate = useNavigate();
